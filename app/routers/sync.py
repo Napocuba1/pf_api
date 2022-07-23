ddc52c269be659bbaa6d5046eb97c2bbf6f4c355
Napoleon <napocuba@gmail.com>
2022-07-23 13:13:27 -0400

    api keys

--- app/routers/sync.py
+++ app/routers/sync.py
@@ -5,16 +5,18 @@ from fastapi import APIRouter, Depends
 from app.models.history import RiegoHistory, TemperaturaHistory
 from app.services.temperatura_history_service import create as create_temp
 from app.services.riego_history_service import create as create_riego
+
+from app.token import get_api_key
 router = APIRouter()
 
 
-@router.post("/temperatura/data", response_model=str)
+@router.post("/temperatura/data", response_model=str, dependencies=[Depends(get_api_key)])
 async def upload_online( item: List[TemperaturaHistory]):
     print (item)
     result = create_temp(item)
     return str(result)
     
-@router.post("/riego/data", response_model=str)
+@router.post("/riego/data", response_model=str, dependencies=[Depends(get_api_key)])
 async def upload_online( item: List[RiegoHistory]):
     print (item)
     result = create_riego(item)
--- app/routers/user.py
+++ app/routers/user.py
@@ -2,9 +2,10 @@ from ast import Str
 from typing import Optional
 from fastapi import APIRouter, Depends
 
+from app.token import get_api_key
 router = APIRouter()
 
 
-@router.get("/me", response_model=str)
+@router.get("/me", response_model=str, dependencies=[Depends(get_api_key)])
 async def get_me():
     return "user"
